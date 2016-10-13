fd2906545edcddd7d7fa2a394b023b87d240fa47
will van wart <william@wvwproductions.com>
2016-10-13 17:59:19 -0400

    rails generate chat_rooms controller, build routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'chat_rooms/index'
+
+  get 'chat_rooms/create'
+
+  get 'chat_rooms/new'
+
+  get 'chat_rooms_controller/index'
+
+  get 'chat_rooms_controller/create'
+
+  get 'chat_rooms_controller/new'
+
+  get 'chat_room_controller/index'
+
+  get 'chat_room_controller/create'
+
+  get 'chat_room_controller/new'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
