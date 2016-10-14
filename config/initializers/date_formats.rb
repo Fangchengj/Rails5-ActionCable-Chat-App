f757d2bc13288c3dedfab85be6bf1e4a380452b4
will van wart <william@wvwproductions.com>
2016-10-14 10:20:03 -0400

    add date formatters to initializers

---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,6 @@
+#date default
+Date::DATE_FORMATS[:default] = "%B %e, %Y "
+#datetime/time default
+Time::DATE_FORMATS[:default] = "%B %e %Y at %l:%M %p"
+#last updated at
+Time::DATE_FORMATS[:time_update] = "at %l:%M %p"
