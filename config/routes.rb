763ebd95b80d8124cf04150acf2f86256709cb03
will van wart <william@wvwproductions.com>
2016-10-14 10:40:23 -0400

    edit chat_rooms routes, set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,25 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
+  #
+  # get 'chat_rooms/index'
+  #
+  # get 'chat_rooms/create'
+  #
+  # get 'chat_rooms/new'
+  #
+  # get 'chat_rooms_controller/index'
+  #
+  # get 'chat_rooms_controller/create'
+  #
+  # get 'chat_rooms_controller/new'
+  #
+  # get 'chat_room_controller/index'
+  #
+  # get 'chat_room_controller/create'
+  #
+  # get 'chat_room_controller/new'
 
-  get 'chat_rooms/index'
-
-  get 'chat_rooms/create'
-
-  get 'chat_rooms/new'
-
-  get 'chat_rooms_controller/index'
-
-  get 'chat_rooms_controller/create'
-
-  get 'chat_rooms_controller/new'
-
-  get 'chat_room_controller/index'
-
-  get 'chat_room_controller/create'
-
-  get 'chat_room_controller/new'
-
+  resources :chat_rooms, only: [:index, :new, :create, :show]
+  root to 'chat_rooms#index'
   devise_for :users, controllers: {registrations: "registrations"}
 end
