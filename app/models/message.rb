566de9bf73c3a709a995cae7e4debeb30174d4a5
will van wart <william@wvwproductions.com>
2016-10-17 11:32:47 -0400

    after create commit, add to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
 class Message < ApplicationRecord
   validates :body, presence: true, length: {minimum: 2, maximum: 1000}
+
   belongs_to :user
   belongs_to :chat_room
+
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
+
 end
