1b9f76579557a07f10935799e3e319153d169b84
will van wart <william@wvwproductions.com>
2016-10-17 10:58:38 -0400

    build chat_rooms_channel & set stream path

---
+++ app/channels/chat_rooms_channel.rb
@@ -0,0 +1,13 @@
+ChatRoomsChannel < ApplicationCable::Channel
+
+  def subscribed
+    stream_from "chat_rooms_#{params['chat_room_id']}_channel"
+  end
+
+  def unsubscribed
+  end
+
+  def send_message(data)
+  end
+
+end
